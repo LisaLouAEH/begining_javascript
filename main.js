ba7f6effdc97a138af6687484e99000a003d7631
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-11-19 21:37:02 +0100

    toujours bug

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// chaque valeur de variable est retrouvé grace a l'ID de l'elements html
 var oneBtn = document.getElementById('calc__un');
 var twoBtn = document.getElementById('calc__deux');
 var threeBtn = document.getElementById('calc__trois');
@@ -15,25 +16,89 @@ var backspaceBtn = document.getElementById('calc__backspace');
 
 var displayValElement = document.getElementById('calc__display__value');
 
+// les valeurs sont retrouvé par le nom de classe, ce qui permet d'en attribuer plusieurs a la variable.
 var calcNumBtns = document.getElementsByClassName('calc__btn__num');
 var calcOperatorBtns = document.getElementsByClassName('calc__btn__operator');
 
-var displayVal = 0; //valeur par defaut 'zero'
+var displayVal = '0'; //valeur par defaut 'zero' mais pourquoi en format string ?
 var pendingVal; // valeur clickée
 var evalStringArray = []; //contient l'operation
 
 var updateDisplayVal = (clickObj) => {
-    var btnText = clickObj.target.innerText;
-    if (displayVal === 0){
-        displayVal = "";
-    }
-    displayVal += btnText;
-    displayValElement.innerText = displayVal;
+var btnText = clickObj.target.innerText;
+if (displayVal === '0'){
+displayVal = "";
+}
+displayVal += btnText;
+displayValElement.innerText = displayVal;
 }
 
-for (let i = 0; i < calcNumBtns.length; i++){
-    calcNumBtns[i].addEventListener('click', updateDisplayVal, false);
+var performOperation = (clickObj)=> {
+var operator = clickObj.target.innerText;
+switch(operator){
+case '+':
+    pendingVal = displayVal;
+    displayVal = '0';
+    displayValElement.innerText = displayVal;
+    evalStringArray.push(pendingVal);
+    evalStringArray.push('+');
+    break;
+case '-':
+    pendingVal = displayVal;
+    displayVal = '0';
+    displayValElement.innerText = displayVal;
+    evalStringArray.push(pendingVal);
+    evalStringArray.push('-');
+    break;
+case 'x':
+    pendingVal = displayVal;
+    displayVal = '0';
+    displayValElement.innerText = displayVal;
+    evalStringArray.push(pendingVal);
+    evalStringArray.push('*'); 
+    break;
+case '/': 
+    pendingVal = displayVal;
+    displayVal = '0';
+    displayValElement.innerText = displayVal;
+    evalStringArray.push(pendingVal);
+    evalStringArray.push('/');
+    break;
+case '=': 
+    evalStringArray.push(displayVal);
+    var evaluation = eval(evalStringArray.join(''));// ['5', '+', '5'] une fois join = ['5+5']
+    displayVal = evaluation + '';
+    displayValElement.innerText = displayVal;
+    evalStringArray = [];
+    break;
+default:
+    break;
 }
-/*for (let i = 0; i < calcOperatorBtns.length; i++){
-    calcOperatorBtns[i].addEventListener('click', performOperation, false)
-}*/
+}
+for (let i = 0; i < calcNumBtns.length; i++){
+calcNumBtns[i].addEventListener('click', updateDisplayVal, false);
+}
+for (let i = 0; i < calcOperatorBtns.length; i++){
+calcOperatorBtns[i].addEventListener('click', performOperation, false)
+}
+clearBtn.onclick = ()=> {
+// reset la calculette a zero
+displayVal = '0';
+pendingVal = undefined;
+evalStringArray = [];
+displayValElement.innerHTML = displayVal;
+}
+backspaceBtn.onclick = ()=> {
+// permet de modifier la dernière saisie saisie
+let lengthOfDisplayVal = displayVal.length;
+displayVal = displayVal.slice(0, lengthOfDisplayVal - 1); 
+if (displayVal === ""){
+displayVal = '0';
+}
+displayValElement.innerText = displayVal; 
+}
+decimalBtn.onclick = ()=> {
+if (!displayVal.includes('.'))
+displayVal += '.';
+displayValElement.innerText = displayVal;
+}
